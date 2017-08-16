17a34dc390eef69fb660ef5009c8928be56ba986
Igor Knop <igorknop@gmail.com>
2017-08-16 00:27:39 -0300

    Initial libgdx project

---
+++ android/src/br/ufjf/dcc/roguecode/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc.roguecode;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.ufjf.dcc.roguecode.RogueCodeAdv;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RogueCodeAdv(), config);
+	}
+}
---
+++ html/src/br/ufjf/dcc/roguecode/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package br.ufjf.dcc.roguecode.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import br.ufjf.dcc.roguecode.RogueCodeAdv;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new RogueCodeAdv();
+        }
+}
